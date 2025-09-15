f5a3a6021d4f4b5abe12000cc4fc60dceafa962e
SR-ING <julioesroma@gmail.com>
2025-09-15 16:52:27 -0500

    Update GLOBAL firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -6,12 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Reemplaza lo siguiente con la configuración de tu propio proyecto de Firebase
 // que obtuviste en el Paso 1.
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAGgtHnpvjjCykFwuvoOz6s0qnT6N4aKiU",
+  authDomain: "gestor-inventario-ia.firebaseapp.com",
+  projectId: "gestor-inventario-ia",
+  storageBucket: "gestor-inventario-ia.firebasestorage.app",
+  messagingSenderId: "627613583623",
+  appId: "1:627613583623:web:5619bc19e1f6e47cc79da2",
+  measurementId: "G-Y64BC1JSEH"
 };
 
 // Inicializa Firebase
